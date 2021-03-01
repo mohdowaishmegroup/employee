5654400676be3501c5082cef88e84e04cd139762
mohdowaishmegroup <79833703+mohdowaishmegroup@users.noreply.github.com>
2021-03-01 15:12:06 +0530

    Update models.py

--- venu/models.py
+++ venu/models.py
@@ -4,5 +4,9 @@ class Empinsert(models.Model):
     empname=models.CharField(max_length=100)
     email=models.CharField(max_length=100)
     country=models.CharField(max_length=100)
+
+    def save(self, **kwargs):
+        super(Empinsert, self).save()
+
     class Meta:
-        db_table:"newemptable"
+        db_table='newemptable'
